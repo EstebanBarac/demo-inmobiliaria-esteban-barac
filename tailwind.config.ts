8d3aee7c2447921e8b8fe139e5f6aab2600ad0b2
Esteban Barac <EstebanBarac8@gmail.com>
2025-02-19 15:16:52 -0300

    update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -50,20 +50,20 @@ export default {
 		  sm: "calc(var(--radius) - 4px)",
 		},
 		keyframes: {
-		  "accordion-down": {
-			from: { height: 0 },
-			to: { height: "var(--radix-accordion-content-height)" },
+			"accordion-down": {
+			  from: { height: "0" }, // Cambia 0 por "0"
+			  to: { height: "var(--radix-accordion-content-height)" },
+			},
+			"accordion-up": {
+			  from: { height: "var(--radix-accordion-content-height)" },
+			  to: { height: "0" }, // Cambia 0 por "0"
+			},
 		  },
-		  "accordion-up": {
-			from: { height: "var(--radix-accordion-content-height)" },
-			to: { height: 0 },
-		  },
-		},
 		animation: {
 		  "accordion-down": "accordion-down 0.2s ease-out",
 		  "accordion-up": "accordion-up 0.2s ease-out",
 		},
 	  },
 	},
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 } satisfies Config;
